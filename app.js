21021be42937d9da0d38e2fb613ee0fbdd705ea9
alireza <alireza.ksv@gmail.com>
2022-01-14 23:23:10 +0330

    added sequelize, create database and migrate tables

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const application = express();
 
 const run = () => {
-    application.listen(8000, () => {
-        console.log("Server is running ...");
-    })
-}
+  application.listen(8000, () => {
+    console.log("Server is running ...");
+  });
+};
 
 module.exports = run;
---
+++ migrations/20220114194047-create-item.js
@@ -0,0 +1,32 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("items", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER.UNSIGNED,
+      },
+      title: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 0,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("items");
+  },
+};
