bf15b9567f2d81db1cf6b4972126ac2a74be0393
alirezakeshvari <alireza.ksv@gmail.com>
2022-01-15 14:33:36 +0330

    added get items list

--- controllers/ItemsController.js
+++ controllers/ItemsController.js
@@ -1,10 +1,20 @@
 const Item = require("../models").Item;
 
 class ItemsController {
-  index(req, res) {
-    res.send({
-      message: "worked",
-    });
+  async getItemsList(req, res) {
+    let items = await Item.findAll();
+    if (items) {
+      res.status(200).send({
+        message: "Items listed successfully",
+        status: 200,
+        items: items,
+      });
+    } else {
+      res.status(200).send({
+        message: "No items founded!",
+        status: 200,
+      });
+    }
   }
 
   async createItem(req, res) {
--- routes/item.js
+++ routes/item.js
@@ -2,6 +2,7 @@ const express = require("express");
 const ItemsController = require("../controllers/ItemsController");
 const router = express.Router();
 
+router.get("/", ItemsController.getItemsList);
 router.post("/", ItemsController.createItem);
 
 module.exports = router;
